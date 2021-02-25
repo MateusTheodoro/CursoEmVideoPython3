c0bf35b298f606d05c1c408d3320afcd65de729d
MateusTheodoro <mateus-jau@hotmail.com>
2021-02-25 15:19:25 -0300

    Mostrar Data de Nascimento
    Programa pergunta dia/mes/ano. Usuário responde e é mostrado na tela

---
+++ ex002 - MostrandoDataNascimento.py
@@ -0,0 +1,4 @@
+dia = input("Escreva o dia que nasceu: ")
+mes = input("Escreva o mês que nasceu: ")
+ano = input("Em que ano você nasceu? ")
+print(f"Você nasceu no dia {dia} do mês de {mes} do ano de {ano}")
