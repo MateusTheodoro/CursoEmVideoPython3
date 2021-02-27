9bbc5526e10123b6f2071cf4c93749d415fd0f2b
MateusTheodoro <mateus-jau@hotmail.com>
2021-02-27 11:50:26 -0300

    Create ex022 - AnalisadorDeTextos.py

---
+++ exercícios/ex022 - AnalisadorDeTextos.py
@@ -0,0 +1,10 @@
+nome = str(input('Digite seu nome completo: '))
+
+maiusc = nome.upper()
+print(f'Upper: {maiusc}')
+
+minusc = nome.lower()
+print(f'Lower: {minusc}')
+
+letras = count.nome
+print(f'Letras: {letras}')
