62e82cb44964708cfec5ee763cd0a02c2268565d
MateusTheodoro <mateus-jau@hotmail.com>
2021-02-28 12:53:54 -0300

    Update ex023 - separandoDigitosDoNro.py

--- exercícios/ex023 - separandoDigitosDoNro.py
+++ exercícios/ex023 - separandoDigitosDoNro.py
@@ -1,8 +1,6 @@
 numero = str(input('Digite um número de 0 a 9999: '))
 
-numeroDiv = numero.split()
-
-print(f'Unidade: {len(numeroDiv[0])}')
-print(f'Dezena: {len(numeroDiv[1])}')
-print(f'Centena: {len(numeroDiv[2])}')
-print(f'Milhar: {len(numeroDiv[3])}')
+print(f'Milhar: {numero[0]}')
+print(f'Centena: {numero[1]}')
+print(f'Dezena: {numero[2]}')
+print(f'Unidade: {numero[3]}')
