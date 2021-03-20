66be196bb559dbcbe33faf4597885f1d4b7d4e78
MateusTheodoro <mateus-jau@hotmail.com>
2021-03-20 19:25:25 -0300

    Exercicios adicionados

--- exercícios/ex022 - AnalisadorDeTextos.py
+++ exercícios/ex022 - AnalisadorDeTextos.py
@@ -1,4 +1,4 @@
-nome = str(input('Digite seu nome completo: '))
+nome = str(input('Digite seu nome completo: ')).strip()
 
 maiusc = nome.upper()
 print(f'Upper: {maiusc}')
@@ -6,8 +6,7 @@ print(f'Upper: {maiusc}')
 minusc = nome.lower()
 print(f'Lower: {minusc}')
 
-semEspaço = nome.replace(' ','')
-print(f'Existem {len(semEspaço)} caracteres sem contar os espaços!')
+print(f'Seu nome tem {len(nome) - nome.count(" ")} letras')
 
 cadaPalavra = nome.split()
 print(f'A primeira palavra tem {len(cadaPalavra[0])} letras e a segunda palavra tem {len(cadaPalavra[1])} letras')
--- exercícios/ex023 - separandoDigitosDoNro.py
+++ exercícios/ex023 - separandoDigitosDoNro.py
@@ -1,6 +1,11 @@
-numero = str(input('Digite um número de 0 a 9999: '))
+numero = int(input('Digite um número de 0 a 9999: '))
 
-print(f'Milhar: {numero[0]}')
-print(f'Centena: {numero[1]}')
-print(f'Dezena: {numero[2]}')
-print(f'Unidade: {numero[3]}')
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
