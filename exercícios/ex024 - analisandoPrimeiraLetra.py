77f4dbd066d8665462197833f83fb536f120359f
MateusTheodoro <mateus-jau@hotmail.com>
2021-04-27 12:26:27 -0300

    Novos
    Novos ex

--- exercícios/ex024 - analisandoPrimeiraLetra.py
+++ exercícios/ex024 - analisandoPrimeiraLetra.py
@@ -1,6 +1,3 @@
-cidade = str(input('Digite o nome da cidade: '))
-maiusc = cidade.upper() #transformando a frase em maiúscula
-dividindo = maiusc.split() #dividindo a frase que foi convertida em maiúscula
-procurando = 'SANTO' in dividindo[0] #procurando 'SANTO' na primeira palavra da frase
+cid = str(input('Digite o nome da cidade: ')).strip()
+print(cid[:5].upper() == "SANTO")
 
-print(f'A cidade {maiusc} começa com "SANTO"?\n"True" para verdadeiro. "False" para falso\n{procurando}')
---
+++ exercícios/ex025 - procurandoSilva.py
@@ -0,0 +1,3 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+
+print(f'Seu nome tem Silva? {"SILVA" in nome.upper()}')
